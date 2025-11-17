ded59e98a9ec41f081ff7ea6bb2b441f2ee610e0
sab18 <sarabarrows18@gmail.com>
2025-11-17 17:11:18 -0500

    add v1.0.1

--- executable.py
+++ executable.py
@@ -10,7 +10,7 @@ PyInstaller.__main__.run([
     'app.py',
     '--onefile',
     '--windowed',
-    '--name', 'MyTimesheet_v1.0.0',
+    '--name', 'MyTimesheet_v1.0.1',
 
     '--icon', 'schrodinger_icon.ico',
 ])
